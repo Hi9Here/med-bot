3bc20d9e88c9711f8d9c1017245bff4c66df9068
woisme <mywoisme@gmail.com>
2019-01-02 17:43:46 +0000

    added Welcome

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,10 @@
 const functions = require('firebase-functions');
 // Add more packages as you need them
 const {
-  dialogflow
+  dialogflow,
+  SimpleResponse,
+  Suggestions,
+  Permission
 } = require('actions-on-google');
 const admin = require('firebase-admin');
 
@@ -31,3 +34,17 @@ const app = dialogflow({
   clientId: functions.config().fireconfig.id,
   debug: true,
 });
+
+// Handle the Dialogflow intent named 'Default Welcome Intent'.
+app.intent('Default Welcome Intent', (conv) => {
+  // Asks the user's permission to know their name, for personalization.
+  conv.ask(new Permission({
+    context: `Hi, this is the Medicine Reminder Bot bot, can we please record your devices current location?`,
+    permissions: ['DEVICE_COARSE_LOCATION'],
+  }))
+  conv.ask(new SimpleResponse('Choose'))
+  conv.ask(new Suggestions([`Yes`, 'Cancel']));
+  return
+})
+
+exports.medbot = functions.https.onRequest(app);
