3a51759b58e4364b6ea4832046d33c0e93577a6b
woisme <mywoisme@gmail.com>
2019-01-08 09:41:17 +0000

    added med-bot

--- functions/index.js
+++ functions/index.js
@@ -24,7 +24,7 @@ const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
 
 // Version and logging
-const version = 0.261;
+const version = 0.27;
 
 const datetime = Date.now();
 const when = moment(datetime).format('MMMM Do YYYY, h:mm:ss a');
@@ -97,47 +97,45 @@ app.intent("Start Sign-in", conv => {
 });
 // End Sign In
 
-// Get Sign In
-app.intent("Get Sign In", (conv, params, signin) => {
-  const { payload } = conv.user.profile
-  console.info(`* Get Sign In * Fired`);
-  console.info(`* Get Sign In * signin.status is ${JSON.stringify(signin.status, null, 2)}`);
-  console.info(`* Get Sign In * payload const is ${JSON.stringify(payload, null, 2)}`);
-  if (signin.status === "OK") {
-    conv.ask("Sign-in Done.");
-    conv.ask(new Suggestions([`Setup`]))
-    console.info(`* Get Sign In * Sign-in Done Fired`);
-    return
-  } else {
-    conv.ask("You need to sign in to use me.");
-    console.info(`* Get Sign In * You need to sign in to use me fired`);
-    return
-  }
-});
-// End Get Sign In
+// // Get Sign In
+// app.intent("Get Sign In", (conv, params, signin) => {
+//   const { payload } = conv.user.profile
+//   console.info(`* Get Sign In * Fired`);
+//   console.info(`* Get Sign In * signin.status is ${JSON.stringify(signin.status, null, 2)}`);
+//   console.info(`* Get Sign In * payload const is ${JSON.stringify(payload, null, 2)}`);
+//   if (signin.status === "OK") {
+//     conv.ask("Sign-in Done.");
+//     conv.ask(new Suggestions([`Setup`]))
+//     console.info(`* Get Sign In * Sign-in Done Fired`);
+//     return
+//   } else {
+//     conv.ask("You need to sign in to use me.");
+//     console.info(`* Get Sign In * You need to sign in to use me fired`);
+//     return
+//   }
+// });
+// // End Get Sign In
 
 // Taken Medicine
 app.intent('Taken Medicine', (conv, params, signin) => {
-  console.info(`* Taken Medicine * signin.status is ${JSON.stringify(signin.status, null, 2)}`);
-  conv.ask(new SimpleResponse(`Thank you`))
-  if (signin.status === "OK") {
-    console.info(`* Taken Medicine * Fired`);
-    return db.collection(`user`).doc(conv.data.uid).set({
-      Taken: datetime
-    })
-    .then (() => {
-      console.info(`* Taken Medicine * taken time and date is ${JSON.stringify(datetime, null, 2)}`);
+    console.info(`* Taken Medicine * signin.status is ${JSON.stringify(signin.status)}`);
+    conv.ask(new SimpleResponse(`Thank you for telling me about taking your medicine`))
+    if (signin.status === "OK") {
+      console.info(`* Taken Medicine * Fired`);
+      db.collection(`user`).doc(conv.data.uid).update({ Taken: datetime })
+        .then(() => {
+          console.info(`* Taken Medicine * taken time and date is ${JSON.stringify(datetime, null, 2)}`);
+          return
+        })
+    } else {
+      conv.ask("You need to sign in to use me.");
+      console.info(`* Taken Medicine * conv.data.uid is ${JSON.stringify(conv.data.uid, null, 2)}`);
+      console.info(`* Taken Medicine * You need to sign in to use me fired`);
       return
-    })
-  } else {
-    conv.ask("You need to sign in to use me.");
-    console.info(`* Taken Medicine * conv.data.uid is ${JSON.stringify(conv.data.uid, null, 2)}`);
-    console.info(`* Taken Medicine * You need to sign in to use me fired`);
-    return
-  }
+    }
 
-})
-// End Taken Medicine
+  })
+  // End Taken Medicine
 
 // // Setup
 // app.intent('Setup', async (conv) => {
